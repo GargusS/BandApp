7ecaa96ba11bcdde6a292cb050ea69f304857f1b
Argus <geirsonnesyn@gmail.com>
2025-11-10 20:05:55 +0100

    restyling av sider og annen layout

--- Models/BandMember.cs
+++ Models/BandMember.cs
@@ -5,6 +5,7 @@ namespace BandApp.Models
     public int Id { get; set; }
     public string Name { get; set; } = "";
     public string Role { get; set; } = "";
+    public string Instrument { get; set; } = "";
     public string ImageUrl { get; set; } = "";
   }
 }
